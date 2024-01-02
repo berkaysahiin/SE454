641276d78ecd31b0cab80a9ae7c75d0c6063560a
Berkay Sahin <berkaysahindev@gmail.com>
2024-01-02 23:52:12 +0300

    update

--- rank_based_coloring2.py
+++ rank_based_coloring2.py
@@ -1,37 +1,20 @@
 from mpi4py import MPI
 import numpy as np
+import time 
 from enum import Enum
-
-comm = MPI.COMM_WORLD
-rank = comm.Get_rank()
-n = comm.Get_size()
-                  #0 1 2 3 4 5 6 7  
-G  =    np.array([[0,0,0,1,1,0,0,1], #0
-                  [0,0,0,0,0,1,0,1], #1
-                  [0,0,0,1,1,0,1,0], #2
-                  [1,0,1,0,1,0,1,0], #3
-                  [1,0,1,1,0,1,0,1], #4
-                  [0,1,0,0,1,0,0,1], #5
-                  [0,0,1,1,0,0,0,0], #6
-                  [1,1,0,0,1,1,0,0]] #7
-                  ,dtype=int)
-           #0   1   2  3   4   5   6  7
-children = [[3],[],[4],[6],[7],[1],[2],[5]]
-           #0 1 2 3 4 5 6 7 
-parents  = [0,5,6,0,2,7,3,4]
 
 class NodeState(Enum):
     IDLE = 0
     COLORED = 1
-    FINISHED = 2
-    TERMINATED = 3
+    TERMINATE = 3
 
 class MessageType(Enum):
     ROUND = 0
-    COLOR = 1
-    DISCARD = 2
-    ROVER = 3
-    FINISH = 4
+    UPDATE = 1
+    ACK = 2
+    DISCARD = 3
+    ROVER = 4
+    TERMINATE = 5
 
 class Node:
     def __init__(self, rank, state=NodeState.IDLE):
@@ -45,21 +28,31 @@ class Node:
         self.round_rcvd = False
         self.color = -1
         self.banned_colors = set()
-        self.colored_childs = set()
-        self.finished_childs = set()
-    
+
     def find_smallest_available_color(self):
         candidate_color = 0
         while candidate_color in self.banned_colors:
             candidate_color += 1
         return candidate_color
-    
+
     def color_self(self, color):
         self.color = color
         self.state = NodeState.COLORED
+    
+    def is_root(self):
+        return node.rank == 0
+
+    def is_colored(self):
+        return node.state == NodeState.COLORED
+
+    def not_colored(self):
+        return node.state != NodeState.COLORED
+
+    def highest_degree_not_colored_neighbors(self):
+        return not node.not_colored_neighbors or node.rank > max(node.not_colored_neighbors)
 
 class Message:
-    def __init__(self, sender, type, data):
+    def __init__(self, sender, type, data=None):
         self.sender = sender
         self.type = type
         self.data = data
@@ -68,90 +61,95 @@ class Alg:
     round_number = -1
     round_over = False
 
+# MPI Initialization
+comm = MPI.COMM_WORLD
+rank = comm.Get_rank()
+n = comm.Get_size()
 
+# Graph and Tree Structures
+ROOT = 0
+G = np.array([[0, 0, 0, 1, 1, 0, 0, 1],
+              [0, 0, 0, 0, 0, 1, 0, 1],
+              [0, 0, 0, 1, 1, 0, 1, 0],
+              [1, 0, 1, 0, 1, 0, 1, 0],
+              [1, 0, 1, 1, 0, 1, 0, 1],
+              [0, 1, 0, 0, 1, 0, 0, 1],
+              [0, 0, 1, 1, 0, 0, 0, 0],
+              [1, 1, 0, 0, 1, 1, 0, 0]], dtype=int)
+
+children = [[3], [], [4], [6], [7], [1], [2], [5]]
+parents = [0, 5, 6, 0, 2, 7, 3, 4]
+
+# Algorithm Execution
 node = Node(rank)
 
-#print(f'rank : {rank} -> {node.neighbors}')
-
-while node.state != NodeState.TERMINATED:
+while node.state != NodeState.TERMINATE:
     Alg.round_over = False
     node.neighs_rcvd.clear()
     node.rovers_rcvd.clear()
     node.round_rcvd = False
 
-    if node.rank == 0: # root node, start next round
-        if len(node.finished_childs) == len(node.childs):
-            #termination
-            #termination_msg = Message(sender=rank, type=MessageType.TERMINATE, data=None)
-            #comm.send(obj=round_msg, dest=0, tag=MessageType.TERMINATE.value)
-            pass
-        else:
+    if node.is_root():
+        if node.is_colored(): ## termination condition
+            term_msg = Message(sender=rank, type=MessageType.TERMINATE)
+            comm.send(obj=term_msg, dest=ROOT, tag=MessageType.TERMINATE.value)
+        else: # start new round
             Alg.round_number += 1
             round_msg = Message(sender=rank, type=MessageType.ROUND, data=Alg.round_number)
-            comm.send(obj=round_msg, dest=0, tag=MessageType.ROUND.value)
-        #print(f'----------------Starting round {Alg.round_number}----------------')
+            comm.send(obj=round_msg, dest=ROOT, tag=MessageType.ROUND.value)
 
     while not Alg.round_over:
         msg: Message = comm.recv(source=MPI.ANY_SOURCE, tag=MPI.ANY_TAG)
-        
+
         if msg.type == MessageType.ROUND:
-            #print(f'Round recieved {node.rank}')
             Alg.round_number = msg.data
             round_msg = Message(sender=rank, type=MessageType.ROUND, data=msg.data)
-            for child in node.childs:
+            for child in node.childs: # broadcast round 
                 comm.send(obj=round_msg, dest=child, tag=MessageType.ROUND.value)
 
-            discard_msg = Message(sender=rank, type=MessageType.DISCARD, data=None)
             if node.state == NodeState.COLORED:
+                discard_msg = Message(sender=rank, type=MessageType.DISCARD, data=None)
                 for neigh in node.neighbors:
                     comm.send(obj=discard_msg, dest=neigh, tag=MessageType.DISCARD.value)
             else:
-                if not node.not_colored_neighbors or node.rank > max(node.not_colored_neighbors):
+                if node.highest_degree_not_colored_neighbors():
                     color = node.find_smallest_available_color()
                     node.color_self(color)
                     update_msg = Message(sender=rank, type=MessageType.UPDATE, data=node.color)
                     for neigh in node.not_colored_neighbors:
                         comm.send(obj=update_msg, dest=neigh, tag=MessageType.UPDATE.value)
                 else:
+                    discard_msg = Message(sender=rank, type=MessageType.DISCARD, data=None)
                     for neigh in node.neighbors:
                         comm.send(obj=discard_msg, dest=neigh, tag=MessageType.DISCARD.value)
-                
-            node.round_rcvd = True
+
+                node.round_rcvd = True
 
         elif msg.type == MessageType.UPDATE:
             node.neighs_rcvd.add(msg.sender)
-            node.banned_colors.add(msg.data)
-
-            if msg.sender in node.not_colored_neighbors:
-                node.not_colored_neighbors.remove(msg.sender)
-
-            #print(f'UPDATE: rank: {node.rank}, colored neighbor: {msg.sender} with color: {msg.data}, sending ack', flush=True)
-
             ack_msg = Message(sender=rank, type=MessageType.ACK, data=None)
             comm.send(obj=ack_msg, dest=msg.sender, tag=MessageType.ACK.value)
+
+            if node.not_colored():
+                node.not_colored_neighbors.remove(msg.sender)
+                node.banned_colors.add(msg.data)
 
         elif msg.type == MessageType.DISCARD:
             node.neighs_rcvd.add(msg.sender)
 
+        elif msg.type == MessageType.ACK:
+            node.neighs_rcvd.add(msg.sender)
+
         elif msg.type == MessageType.ROVER:
             node.rovers_rcvd.add(msg.sender)
-            #print(f'ROVER RECIEVED:  {node.rank} <----- {msg.sender}', flush=True)
-        
-        elif msg.type == MessageType.ACK:
-            #print(f'ACK RECIEVED: rank: {node.rank}, sender: {msg.sender}')
-            node.neighs_rcvd.add(msg.sender)
-        elif msg.type == MessageType.FINISH:
-            node.neighs_rcvd.add(msg.sender)
-            node.finished_childs.add(msg.sender)
-        
+
         elif msg.type == MessageType.TERMINATE:
-            print(f'Recieved terminate msg {node.rank}')
             if node.childs:
                 for child in node.childs:
                     term_msg = Message(sender=rank, type=MessageType.TERMINATE, data=None)
                     comm.send(obj=term_msg, dest=child, tag=MessageType.TERMINATE.value)
 
-            node.state = NodeState.TERMINATED
+            node.state = NodeState.TERMINATE
             break
 
         if node.round_rcvd and node.neighbors.issubset(node.neighs_rcvd) and len(node.childs) == len(node.rovers_rcvd):
@@ -159,12 +157,7 @@ while node.state != NodeState.TERMINATED:
             comm.send(obj=rover_msg, dest=node.parent, tag=MessageType.ROVER.value)
             Alg.round_over = True
 
-
-        if node.state == NodeState.COLORED and (len(node.finished_childs) == len(node.childs) or len(node.childs) == 0):
-            #node is finished
-            node.state = NodeState.FINISHED
-            finished_msg = Message(sender=rank, type=MessageType.FINISH, data=None)
-            comm.send(obj=finished_msg, dest=node.parent, tag=MessageType.FINISH.value)
+print(f'Terminated, rank: {node.rank}, color: {node.color}')
 
 
-print(f'Terminated {node.rank}')
+
